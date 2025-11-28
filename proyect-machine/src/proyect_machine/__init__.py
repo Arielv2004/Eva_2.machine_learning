b1eb8f92d1ef0f624a6b6b8ea7a0511623ca0ab3
Svera00 <severase00@gmail.com>
2025-11-28 13:51:17 -0300

    Subo datos faltantes de DVC

--- proyect-machine/src/proyect_machine/__init__.py
+++ proyect-machine/src/proyect_machine/__init__.py
@@ -1,8 +1,20 @@
-from proyect_machine.pipelines.modelo_regresion import create_pipeline as modelo_regresion_pipeline
+"""Paquete principal del proyecto Kedro."""
+
+# Kedro / setuptools usan esta variable cuando leen pyproject.toml
+__version__ = "0.1.0"
+
 
 def register_pipelines():
+    """Registra los pipelines del proyecto.
+
+    Usamos un import dentro de la función para evitar errores
+    cuando setuptools importa este módulo durante `pip install -e .`.
+    """
+    from proyect_machine.pipelines.modelo_regresion import (
+        create_pipeline as modelo_regresion_pipeline,
+    )
+
     return {
         "modelo_regresion": modelo_regresion_pipeline(),
-
-        "__default__": modelo_regresion_pipeline() 
+        "__default__": modelo_regresion_pipeline(),
     }
